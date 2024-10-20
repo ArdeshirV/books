8a7304b647a6c517668b58ec00041ae70321a6ac
ArdeshirV <e-job@protonmail.com>
2024-10-20 19:16:56 +0330

    Update

--- introducing-go/main.go
+++ introducing-go/main.go
@@ -37,7 +37,7 @@ func ChapterFive() {
 
 	fmt.Println("\nSlices:")
 	myName := "Ardeshir"
-	key := "1"
+	key := "2"
 	res := encode_xor(key, myName)
 	fmt.Println("<", res, ">")
 	res = encode_xor(key, res)
