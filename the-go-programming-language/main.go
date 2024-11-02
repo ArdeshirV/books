4f33b431c3ddee9d8bc3aeb66558b771aebf1e18
ArdeshirV <e-job@protonmail.com>
2024-11-02 17:01:14 +0330

    Update

--- the-go-programming-language/main.go
+++ the-go-programming-language/main.go
@@ -44,10 +44,39 @@ func PrintTitle() {
 	blnColor := true
 	strAppName := "Go.go"
 	strAppYear := "2023"
-	strAppDescription := "Test Golang codes"
+	strAppDescription := "The Go Programming Language"
 	strVersion := "1.0"
 	strLicense := "GPLv3+"
 	strCopyright := "https://github.com/ArdeshirV"
-	print(FormatTitle(strAppName, strAppDescription, strVersion, blnColor))
-	print(FormatCopyright(strAppYear, strCopyright, strLicense, blnColor))
+	fmt.Print(FormatTitle(strAppName, strAppDescription, strVersion, blnColor))
+	fmt.Print(FormatCopyright(strAppYear, strCopyright, strLicense, blnColor))
+}
+
+func FormatTitle(strAppName, strAppDescription, strVersion string, blnColor bool) string {
+	NoneColored := "%v - %v Version %v"
+	Colored := "\033[1;33m%v\033[0;33m - %v \033[1;33mVersion %v\033[0m"
+	var strFormat string
+	if blnColor {
+		strFormat = Colored
+	} else {
+		strFormat = NoneColored
+	}
+	return sprintf(strFormat, strAppName, strAppDescription, strVersion)
+}
+
+func FormatCopyright(strAppYear, strCopyright, strLicense string, blnColor bool) string {
+	NoneColored := "Copyright (c) %v %v, Licensed under %v\n"
+	Colored := ("\033[0;33mCopyright (c) \033[1;33m%v \033[1;34m%v" +
+		"\033[0;33m, Licensed under \033[1;33m%v\033[0m\n")
+	var strFormat string
+	if blnColor {
+		strFormat = Colored
+	} else {
+		strFormat = NoneColored
+	}
+	return sprintf(strFormat, strAppYear, strCopyright, strLicense)
+}
+
+func sprintf(format string, args ...interface{}) string {
+	return fmt.Sprintf(format, args...)
 }
