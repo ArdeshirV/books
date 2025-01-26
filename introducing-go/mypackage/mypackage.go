31d19f02d45b4dbeaaf2e4bf42af1cf5fadd1f5f
ArdeshirV <e-job@protonmail.com>
2025-01-26 06:15:07 +0330

    Update

--- introducing-go/mypackage/mypackage.go
+++ introducing-go/mypackage/mypackage.go
@@ -9,3 +9,33 @@ func printMessage(message string) {
 func Display(msg string) {
 	printMessage(msg)
 }
+
+// Returns minimum of number slice and returns zero if the slice is empty
+func Min(numbers []float64) (Minimum float64) {
+	if numbers == nil || len(numbers) <= 0 {
+		Minimum = 0
+		return Minimum
+	}
+	Minimum = numbers[0]
+	for i := 1; i < len(numbers); i++ {
+		if Minimum > numbers[i] {
+			Minimum = numbers[i]
+		}
+	}
+	return Minimum
+}
+
+// Returns maximum of number slice and returns zero if the slice is empty
+func Max(numbers []float64) (Maximum float64) {
+	if numbers == nil || len(numbers) <= 0 {
+		Maximum = 0
+		return Maximum
+	}
+	Maximum = numbers[0]
+	for i := 1; i < len(numbers); i++ {
+		if Maximum < numbers[i] {
+			Maximum = numbers[i]
+		}
+	}
+	return Maximum
+}
