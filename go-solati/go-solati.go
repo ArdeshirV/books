152aea07ab903977d0892837037422ff15c69f61
ArdeshirV <e-job@protonmail.com>
2025-02-15 07:54:16 +0330

    Update

--- go-solati/go-solati.go
+++ go-solati/go-solati.go
@@ -5,12 +5,28 @@ import (
 	"fmt"
 )
 
+const (
+	NORMAL   = "\033[0m"
+	BOLD     = "\033[1m"
+	RED      = "\033[0;31m"
+	TEAL     = "\033[0;36m"
+	WHITE    = "\033[0;37m"
+	BLUE     = "\033[0;34m"
+	GREEN    = "\033[0;32m"
+	YELLOW   = "\033[0;33m"
+	MAGENTA  = "\033[0;35m"
+	BRED     = "\033[1;31m"
+	BBLUE    = "\033[1;34m"
+	BTEAL    = "\033[1;36m"
+	BWHITE   = "\033[1;37m"
+	BGREEN   = "\033[1;32m"
+	BYELLOW  = "\033[1;33m"
+	BMAGENTA = "\033[1;35m"
+)
+
 func main() {
 	defer func() {
-		fmt.Printf("%s\nPress enter to quit.", NORMAL)
-
-		var tmp string
-		fmt.Scanf("%s", &tmp)
+		//waiteForEnter()
 	}()
 	PrintTitle()
 	bookName := "The Go programming language reference"
@@ -23,4 +39,51 @@ func main() {
 func mainChapterOne() {
 	fmt.Println("  Chapter One: ")
 
+}
+
+func waiteForEnter() {
+	fmt.Printf("%s\nPress enter to quit.", NORMAL)
+	var tmp string
+	fmt.Scanf("%s", &tmp)
+}
+
+func PrintTitle() {
+	blnColor := true
+	strAppName := "go-solati"
+	strAppYear := "2025"
+	strAppDescription := "The Go Programming Language"
+	strVersion := "1.0"
+	strLicense := "GPLv3+"
+	strCopyright := "https://github.com/ArdeshirV/book/go-solati"
+	fmt.Print(FormatTitle(strAppName, strAppDescription, strVersion, blnColor))
+	fmt.Print(FormatCopyright(strAppYear, strCopyright, strLicense, blnColor))
+}
+
+func FormatTitle(strAppName, strAppDescription, strVersion string, blnColor bool) string {
+	NoneColored := "%v - %v Version %v\n"
+	Colored := "\033[1;33m%v\033[0;33m - %v \033[1;33mVersion %v\033[0m\n"
+	var strFormat string
+	if blnColor {
+		strFormat = Colored
+	} else {
+		strFormat = NoneColored
+	}
+	return sprintf(strFormat, strAppName, strAppDescription, strVersion)
+}
+
+func FormatCopyright(strAppYear, strCopyright, strLicense string, blnColor bool) string {
+	NoneColored := "Copyright (c) %v %v, Licensed under %v\n"
+	Colored := ("\033[0;33mCopyright (c) \033[1;33m%v \033[1;34m%v" +
+		"\033[0;33m, Licensed under \033[1;33m%v\033[0m\n")
+	var strFormat string
+	if blnColor {
+		strFormat = Colored
+	} else {
+		strFormat = NoneColored
+	}
+	return sprintf(strFormat, strAppYear, strCopyright, strLicense)
+}
+
+func sprintf(format string, args ...interface{}) string {
+	return fmt.Sprintf(format, args...)
 }
