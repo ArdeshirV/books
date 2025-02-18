79b31d28cb912a71b54e709c949b345fafcaac08
ArdeshirV <e-job@protonmail.com>
2025-02-18 19:40:19 +0330

    Update

--- go-solati/go-solati.go
+++ go-solati/go-solati.go
@@ -52,7 +52,8 @@ in multilines.`)
 	fmt.Println(a, b, c, e)
 
 	var bit1 uint8 = 0b11101101
-	fmt.Printf("%b, %b", bit1, ^bit1)
+	fmt.Printf("%b, %b\n", bit1, ^bit1)
+	fmt.Println("استراحتِ مطلق")
 }
 
 func waiteForEnter() {
