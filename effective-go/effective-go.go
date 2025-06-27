a47b59fb4ccc4d0379ab004379c11cef7cae05ed
ArdeshirV <e-job@protonmail.com>
2025-06-27 11:15:15 +0330

    Update

--- effective-go/effective-go.go
+++ effective-go/effective-go.go
@@ -1,1 +1,34 @@
-Written by Fyne demo
+package main
+
+import (
+	"fmt"
+)
+
+func printTitle(title string) {
+	fmt.Printf("\n  \033[1;36m%s\033[0;36m\n", title)
+}
+
+func printMainTitle() {
+	fmt.Print("\n    \033[1;35mMy practices about ",
+		"\033[0;32m\"Effective Go\" \033[1;32mʕ◔ϖ◔ʔ\033[0;36m\n")
+}
+
+func main() {
+	defer func() {
+		fmt.Print("\033[0m")
+	}()
+	printMainTitle()
+
+	stepOne()
+	stepTwo()
+}
+
+func stepOne() {
+	printTitle("stepOne")
+	fmt.Println("The output goes here. Here we try to practice Go programming language.")
+}
+
+func stepTwo() {
+	printTitle("stepTwo")
+	fmt.Println("The output of stepTwo also goes here.")
+}
