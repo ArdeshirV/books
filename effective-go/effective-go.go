1306eb5e930c452c936149c7d514098ab5bb778c
ArdeshirV <e-job@protonmail.com>
2025-07-12 06:55:38 +0330

    Update
    Co-authored-by: ArdeshirV <e-job@protonmail.com>
    Co-authored-by: electronicjob <electronic-job@protonmail.com>

--- effective-go/effective-go.go
+++ effective-go/effective-go.go
@@ -294,4 +294,4 @@ func (ce *CustomEntity) SetOwner(owner string) {
 func (ce CustomEntity) String() string {
 	return ce.owner
 }
-//123456789101112131415161718192021222324
+//12345678910111213141516171819202122232425
